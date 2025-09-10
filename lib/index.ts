ce287f6088b378205b622d50205c5db200927524
amediantsev <zencrazycat@gmail.com>
2025-09-10 18:24:00 +0300

    Add package export for Builder from builder.js

---
+++ lib/index.ts
@@ -0,0 +1,8 @@
+export { Builder } from './builder.js';
+export { DataLoader } from './data-loader.js';
+export { TemplateRenderer } from './template-renderer.js';
+export { AssetManager } from './asset-manager.js';
+export { Logger } from './logger.js';
+export { Minifier } from './minifier.js';
+export { ConfigLoader } from './config-loader.js';
+export type { BuilderOptions, TemplateData } from '../types/property.js';
